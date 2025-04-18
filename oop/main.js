3bc95ea6fc4111bb378dd7b2695e3e983fa19e9e
Patrik <patsboy06@gmail.com>
2025-04-18 16:11:05 +0200

    oop refactor 1

--- oop/main.js
+++ oop/main.js
@@ -2,4 +2,17 @@ const separator = document.createElement('hr'); //hogy a html-en egyszeruen megt
 document.body.appendChild(separator); //separator hozzáadása a bodyhoz
  
 const table = new Table("table"); //Table osztály példányosítása, új objektum létrehozása a table classal
-const form = new Form("form"); //Form osztály példányosítása, új objektum létrehozása a form classal
+
+const fieldConfig = [{ //tömb létrehozása, benne 3 objektummal
+    fieldid: 'szerzo', //1. objektum idja
+    fieldLabel: 'szerző' //1. objektum labelje
+},
+{
+    fieldid: 'mufaj', //2. objektum idja
+    fieldLabel: 'műfaj' //2. objektum labelje
+},
+{
+    fieldid: 'cim', //3. objektum idja
+    fieldLabel: 'cím' //3. objektum labelje
+}];
+const form = new Form("form", fieldConfig); //Form osztály példányosítása, új objektum létrehozása a form classal
